fc94ee68d815b2b3f19e9d5163a71530d3664e98
khai96_ <hvksmr1996@gmail.com>
2016-09-26 14:02:56 +0700

    Create remake-package-json script

---
+++ sh/remake-package-json.js
@@ -0,0 +1,10 @@
+'use strict'
+const file = require('path').join(__dirname, '../package.json')
+const {readFileSync, writeFileSync} = require('fs')
+const {stdout} = require('process')
+const {indent, crlf} = require('./lib/package-json-config.json')
+const {parse, stringify} = JSON
+const ENCODING = {encoding: 'utf8'}
+const jsonobj = parse(readFileSync(file, ENCODING))
+const jsonstr = stringify(jsonobj, undefined, indent).split('\n').join(crlf ? CRLF : LF)
+writeFileSync(file, jsonstr, ENCODING)
