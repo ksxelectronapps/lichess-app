d21099e1fa578cced9e12fbcb5ac9e9a97ba42fc
khai96_ <hvksmr1996@gmail.com>
2016-09-26 12:56:49 +0700

    Remove unnecessary lines

--- sh/test.sh
+++ sh/test.sh
@@ -8,9 +8,4 @@
     cat stdout.tmp
     exit 2
   )
-) && (
-  export TEST_COUNT_LOGS="$(pwd)/test-count.tmp"
-  export TEST_COUNT_LOGS_DELEMITER=$(node "$(pwd)/sh/lib/test-count-log.js")
-  echo "Running '/test/index.js'..."
-  node "$(pwd)/test" && node "$(pwd)/test/verify.js"
 )
