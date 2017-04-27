f3d5e1636cdae4a39dab887e820dcbfd2897b6c2
khai96_ <hvksmr1996@gmail.com>
2017-04-28 00:43:30 +0700

    Complete deployment scripts

---
+++ sh/deploy.sh
@@ -0,0 +1,18 @@
+export GH_TOKEN=$GITHUB_RELEASE_OAUTH
+export OPTIONS="publish linux mac win $OPTIONS"
+
+if [[ -z $GH_TOKEN ]]
+  then
+    echo "No Release token provided." >&2
+    echo "Deployment failed." >&2
+    exit 1
+fi
+
+npm run pack && (
+  echo "Deployment succeed."
+) || (
+  status=$?
+  echo "Failed on 'npm run pack'" >&2
+  echo "Deployment failed." >&2
+  exit $status
+)
