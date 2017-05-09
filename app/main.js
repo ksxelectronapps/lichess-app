64ffcc9ee52449ee2abbec1c1a5b44947f23171b
khai96_ <hvksmr1996@gmail.com>
2017-05-09 08:43:09 +0700

    Complete: Lichess icon

--- app/main.js
+++ app/main.js
@@ -1,10 +1,11 @@
 'use strict'
 const {app, BrowserWindow} = require('electron')
 const {platform} = require('process')
+const {icon} = require('./lib/platform.js')
 let mainWindow
 
 function createWindow () {
-  mainWindow = new BrowserWindow()
+  mainWindow = new BrowserWindow({icon})
   mainWindow.loadURL('https://lichess.org')
   mainWindow.on('closed', () => { mainWindow = null })
 }
