f5b996ba896cfde1ff9cae6f89a662f7b3becda3
khai96_ <hvksmr1996@gmail.com>
2017-05-14 01:56:06 +0700

    Split CI jobs

---
+++ sh/ci/pack.sh
@@ -0,0 +1,15 @@
+(
+  echo '-- Installing addons...'
+  bash addons/main.sh
+) && (
+  echo '-- Creating packages...'
+  OPTIONS="publish=never $TARGET_OS $TARGET_ARCH $OPTIONS" npm run pack
+) && (
+  echo '-- Listing artifacts...'
+  ls dist/{,linux,mac,win}
+  true
+) || (
+  status=$?
+  echo "Executation of command '$*' failed." >&2
+  exit $status
+)
