1408ec4d9ec56c22a93ed3130f010df032dfe1aa
khai96_ <hvksmr1996@gmail.com>
2017-05-13 23:54:13 +0700

    Set GH_TOKEN for everything

--- sh/pack.sh
+++ sh/pack.sh
@@ -1,3 +1,10 @@
 npm test && (
+  export GH_TOKEN=$(
+    [ -n $GH_TOKEN ] && (
+      echo $GH_TOKEN
+    ) || (
+      echo $GITHUB_RELEASE_OAUTH
+    )
+  )
   build $(node ./sh/lib/get-options.js)
 )
